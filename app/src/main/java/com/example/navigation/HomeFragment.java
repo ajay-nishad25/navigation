1afadda3b064923dbf8b8aa21824f04aad552615
AjayBholaNishad <ajaynishad4123@gmail.com>
2021-10-16 16:38:35 +0530

    Adding imageview and listener on it

--- app/src/main/java/com/example/navigation/HomeFragment.java
+++ app/src/main/java/com/example/navigation/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,11 +13,22 @@ import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
 
+    ImageView imageView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        imageView = view.findViewById(R.id.imageview);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Hello", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 
 }
