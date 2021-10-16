a94246b920edd86c836280257345e3b8606d1414
AjayBholaNishad <ajaynishad4123@gmail.com>
2021-10-16 17:00:41 +0530

    Adding Textview and listener on it

--- app/src/main/java/com/example/navigation/HomeFragment.java
+++ app/src/main/java/com/example/navigation/HomeFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -14,6 +15,7 @@ import androidx.fragment.app.Fragment;
 public class HomeFragment extends Fragment {
 
     ImageView imageView;
+    TextView textView;
 
     @Nullable
     @Override
@@ -25,6 +27,13 @@ public class HomeFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getContext(), "Hello", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "TextView hello", Toast.LENGTH_SHORT).show();
             }
         });
 
